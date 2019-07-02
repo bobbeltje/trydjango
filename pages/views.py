dab3755447a71b985d6c7b52670d02b09006845f
Matthias <matthiaswubs@gmail.com>
2019-07-02 23:13:16 +0200

    see what's passed to a page request

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
+	print(request)
+	print(request.user)
+	print(args, kwargs)
 	return HttpResponse("<h1>Hello World</h1>")
 
 def contact_view(*args, **kwargs):
 	return HttpResponse('<h1>Contact page</h1>')
-	
