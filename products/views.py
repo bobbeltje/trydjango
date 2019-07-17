ce8cd4d040ed0cdc9e6c5dcbc069ec1b52e96560
Matthias <matthiaswubs@gmail.com>
2019-07-17 18:00:27 +0200

    step back; building raw html form

--- products/views.py
+++ products/views.py
@@ -5,15 +5,19 @@ from .forms import ProductForm
 from .models import Product
 
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
-
-    context = {
-        'form': form
-    }
+    context = {}
     return render(request, 'products/product_create.html', context)
+
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, 'products/product_create.html', context)
 
 
 # Create your views here.
