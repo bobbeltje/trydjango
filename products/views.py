cdb09672fde2152f132e90167717344c7b5e12db
Matthias <matthiaswubs@gmail.com>
2019-07-20 10:10:06 +0200

    back to django form

--- products/views.py
+++ products/views.py
@@ -4,21 +4,21 @@ from .forms import ProductForm, RawProductForm
 
 from .models import Product
 
-def product_create_view(request):
-    my_form = RawProductForm()
-    if request.method == 'POST':
-        my_form = RawProductForm(request.POST)
-        if my_form.is_valid():
-            # now the data is good
-            print(my_form.cleaned_data)
-            Product.objects.create(**my_form.cleaned_data)
-        else:
-            print(my_form.errors)
+# def product_create_view(request):
+#     my_form = RawProductForm()
+#     if request.method == 'POST':
+#         my_form = RawProductForm(request.POST)
+#         if my_form.is_valid():
+#             # now the data is good
+#             print(my_form.cleaned_data)
+#             Product.objects.create(**my_form.cleaned_data)
+#         else:
+#             print(my_form.errors)
 
-    context = {
-        'form': my_form
-    }
-    return render(request, 'products/product_create.html', context)
+#     context = {
+#         'form': my_form
+#     }
+#     return render(request, 'products/product_create.html', context)
 
 # def product_create_view(request):
 #     # print(request.GET)
@@ -30,16 +30,16 @@ def product_create_view(request):
 #     context = {}
 #     return render(request, 'products/product_create.html', context)
 
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
 
-#     context = {
-#         'form': form
-#     }
-#     return render(request, 'products/product_create.html', context)
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
 
 
 # Create your views here.
