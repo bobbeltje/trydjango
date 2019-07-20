a1929958f0a006427eb68a75d1869182fffd2ef8
Matthias <matthiaswubs@gmail.com>
2019-07-20 11:03:17 +0200

    set initial data and edit model form data (not working)

--- products/views.py
+++ products/views.py
@@ -4,6 +4,19 @@ from .forms import ProductForm, RawProductForm
 
 from .models import Product
 
+def render_initial_data(request):
+    initial_data = {
+        'title': 'My initial title'
+    }
+    obj = Products.objects.get(id=1)
+    form = ProductForm(request.POST or None, initial=initial_data, instance=obj)
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+    return render(request, 'products/product_create.html', context)
+
 # def product_create_view(request):
 #     my_form = RawProductForm()
 #     if request.method == 'POST':
