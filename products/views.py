6d128205c49c328a9df9a61996d2e853ae699b99
Matthias <matthiaswubs@gmail.com>
2019-07-20 11:36:30 +0200

    delete forms and redirect

--- products/views.py
+++ products/views.py
@@ -1,9 +1,22 @@
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import ProductForm, RawProductForm
 
 from .models import Product
+
+def product_delete_view(request, id):
+    print('oi')
+    obj = get_object_or_404(Product, id=id)
+    if request.method == 'POST':
+        # confirming delete
+        obj.delete()
+        return redirect('../')
+
+    context = {
+        'object': obj
+    }
+    return render(request, 'products/product_delete.html', context)
 
 def dynamic_lookup_view(request, id):
     # obj = Product.objects.get(id=id)
@@ -12,7 +25,7 @@ def dynamic_lookup_view(request, id):
         obj = Product.objects.get(id=id)
     except Product.DoesNotExist:
         raise Http404
-        
+
     context = {
         'object': obj
     }
