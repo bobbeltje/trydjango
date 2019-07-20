570b421fecf48f2ef5ffab671e1a7e1fcc2ad05a
Matthias <matthiaswubs@gmail.com>
2019-07-20 19:13:04 +0200

    article model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=120) # max_length required
+    tags = models.TextField(blank=True, null=True)
+    summary = models.TextField(blank=True, null=False)
+    
