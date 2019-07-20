36dd0946fc7dcf3f883b668d9e7b491014647817
Matthias <matthiaswubs@gmail.com>
2019-07-20 15:55:17 +0200

    django url reverse

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -30,7 +30,7 @@ from products.views import (
 urlpatterns = [
     path('products/', product_list_view, name='product-list'),
     path('products/<int:id>/delete/', product_delete_view, name='product-delete'),
-    path('products/<int:id>', dynamic_lookup_view, name='product'),
+    path('products/<int:id>', dynamic_lookup_view, name='product-detail'),
     path('', home_view, name='home'),
     path('contact/', contact_view),
     path('about/', about_view),
