754544666f20c404c2d534b73ffbf4683eab7eff
Matthias <matthiaswubs@gmail.com>
2019-07-20 10:26:12 +0200

    email input and validation

--- products/forms.py
+++ products/forms.py
@@ -9,6 +9,7 @@ class ProductForm(forms.ModelForm):
         label='', 
         widget=forms.TextInput(attrs={'placeholder': 'The title'})
     )
+    email = forms.EmailField()
     description = forms.CharField(
         required=False, 
         widget=forms.Textarea(
@@ -31,10 +32,15 @@ class ProductForm(forms.ModelForm):
 
     def clean_title(self, *args, **kwargs):
         title = self.cleaned_data.get('title')
-        if 'CFE' in title:
-            return title
-        else:
+        if not 'CFE' in title:
             raise forms.ValidationError('This is not a valid title')
+        return title
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get('email')
+        if not email.endswith('edu'):
+            raise forms.ValidationError('This is not a valid email')
+        return email
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'The title'}))
