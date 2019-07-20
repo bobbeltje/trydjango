1661ae8869e81e91c5943a18577d8ae8a03c1a7e
Matthias <matthiaswubs@gmail.com>
2019-07-20 10:20:22 +0200

    validate form input

--- products/forms.py
+++ products/forms.py
@@ -29,6 +29,13 @@ class ProductForm(forms.ModelForm):
             'price'
         ]
 
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        if 'CFE' in title:
+            return title
+        else:
+            raise forms.ValidationError('This is not a valid title')
+
 class RawProductForm(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={'placeholder': 'The title'}))
     description = forms.CharField(
