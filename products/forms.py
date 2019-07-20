b2eee9cc3846bdf824a1be17b792b35f2236f3be
Matthias <matthiaswubs@gmail.com>
2019-07-20 10:17:23 +0200

    add the raw modifications to the form

--- products/forms.py
+++ products/forms.py
@@ -5,6 +5,22 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(
+        label='', 
+        widget=forms.TextInput(attrs={'placeholder': 'The title'})
+    )
+    description = forms.CharField(
+        required=False, 
+        widget=forms.Textarea(
+            attrs={
+                'Class': 'new-class-name two',
+                'id': 'my-id',
+                'rows': 10,
+                'cols': 50
+            }
+        )
+    )
+    price = forms.DecimalField(initial=199.99)
     class Meta:
         model = Product
         fields = [
