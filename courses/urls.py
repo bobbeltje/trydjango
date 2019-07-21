e70369692f4b1b6e9380ddcbc5766212d784fa5b
Matthias <matthiaswubs@gmail.com>
2019-07-21 16:02:18 +0200

    raw course update view

--- courses/urls.py
+++ courses/urls.py
@@ -3,6 +3,7 @@ from .views import (
     CourseListView,
     CourseView,
     CourseCreateView,
+    CourseUpdateView,
     # my_fbv
     )
 
@@ -10,8 +11,7 @@ app_name = 'courses'
 urlpatterns = [
     path('create/', CourseCreateView.as_view(), name='course-create'),
     path('', CourseListView.as_view(), name='course-list'),
-    # path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
-    path('<int:id>/', CourseView.as_view(), name='course-detail')
-    # path('', my_fbv, name='courses-list'),
+    path('<int:id>/', CourseView.as_view(), name='course-detail'),
+    path('<int:id>/update/', CourseUpdateView.as_view(), name='course-update')
 
 ]
