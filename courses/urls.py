75a8f33fefffcf473a36c67c84c38d39583f03a6
Matthias <matthiaswubs@gmail.com>
2019-07-21 14:24:34 +0200

    overwriting class default template by passing argument

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import (
-    # CourseView,
-    my_fbv
+    CourseView,
+    # my_fbv
     )
 
 app_name = 'courses'
 urlpatterns = [
-    path('', my_fbv, name='courses-list')
+    path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
+    # path('', my_fbv, name='courses-list'),
 
     # path('', product_list_view, name='product-list'),
     # path('<int:id>/delete/', product_delete_view, name='product-delete'),
--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
-def my_fbv(request, *args, **kwargs):
-    return render(request, 'about.html', {})
+# BASE VIEW Class = View
+
+class CourseView(View):
+    template_name = 'about.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {})
+
+# HTTP METHODS
+# def my_fbv(request, *args, **kwargs):
+#     return render(request, 'about.html', {})
