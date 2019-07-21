7996a96d071614b10b82cf09af20f2c11d20d2d6
Matthias <matthiaswubs@gmail.com>
2019-07-21 14:16:05 +0200

    function based view for courses added

---
+++ courses/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (
+    # CourseView,
+    my_fbv
+    )
+
+app_name = 'courses'
+urlpatterns = [
+    path('', my_fbv, name='courses-list')
+
+    # path('', product_list_view, name='product-list'),
+    # path('<int:id>/delete/', product_delete_view, name='product-delete'),
+    # path('<int:id>', dynamic_lookup_view, name='product-detail'),
+    # path('create/', product_create_view),
+    # path('product/', product_detail_view),
+]
