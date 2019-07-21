d2f65f0ac09e85c2f68a3e4f04cdde95365ad543
Matthias <matthiaswubs@gmail.com>
2019-07-21 16:08:29 +0200

    raw delete class based view

--- courses/urls.py
+++ courses/urls.py
@@ -4,12 +4,14 @@ from .views import (
     CourseView,
     CourseCreateView,
     CourseUpdateView,
+    CourseDeleteView,
     # my_fbv
     )
 
 app_name = 'courses'
 urlpatterns = [
     path('create/', CourseCreateView.as_view(), name='course-create'),
+    path('<int:id>/delete/', CourseDeleteView.as_view(), name='course-delete'),
     path('', CourseListView.as_view(), name='course-list'),
     path('<int:id>/', CourseView.as_view(), name='course-detail'),
     path('<int:id>/update/', CourseUpdateView.as_view(), name='course-update')
