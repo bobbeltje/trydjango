1c8ccc758292b4b4371d55ca95ae8ae2bef1fa16
Matthias <matthiaswubs@gmail.com>
2019-07-21 14:36:47 +0200

    raw detail class based view

--- courses/views.py
+++ courses/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import Course
 # BASE VIEW Class = View
 
 class CourseView(View):
     template_name = 'courses/course_detail.html'
     def get(self, request, id=None, *args, **kwargs):
-        return render(request, self.template_name, {})
+        context = {}
+        if id is not None:
+            obj = get_object_or_404(Course, id=id)
+            context['object'] = obj
+        return render(request, self.template_name, context)
 
 # HTTP METHODS
 # def my_fbv(request, *args, **kwargs):
