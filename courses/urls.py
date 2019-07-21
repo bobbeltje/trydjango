bed36106a5cd5e572d34e3b55f37b6397004e8ac
Matthias <matthiaswubs@gmail.com>
2019-07-21 14:47:17 +0200

    raw list class based view

--- courses/urls.py
+++ courses/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .views import (
+    CourseListView,
     CourseView,
     # my_fbv
     )
 
 app_name = 'courses'
 urlpatterns = [
-    path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
+    path('', CourseListView.as_view(), name='course-list'),
+    # path('', CourseView.as_view(template_name='contact.html'), name='courses-list'),
     path('<int:id>/', CourseView.as_view(), name='course-detail')
     # path('', my_fbv, name='courses-list'),
 
